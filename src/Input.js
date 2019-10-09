6628a0717f3c63f829b7bcff4fce5c42d2acf1d4
Maisa Souza Barros <maisa.barros@fazenda.mg.gov.br>
2019-10-09 13:51:55 -0300

    Complete input component

--- src/Input.js
+++ src/Input.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const Input = props => {
+    const contents = props.success ? null : (
+        <form className="form-inline">
+            <input data-test="input-box" className="mb-2 mx-sm-3" type="text" placeholder="enter guess"/>
+            <button data-test="submit-button" type="submit" className="btn btn-primary mb-2">
+                Submit
+            </button>
+        </form>
+    )
     return (
-        <div></div>
+        <div data-test="component-input">
+            {contents}
+        </div>
     )
 }
 
-const mapStateToProps = (state) => {
-    return {}
+const mapStateToProps = ({success}) => {
+    return {
+        success
+    }
 }
 
 
--- src/Input.test.js
+++ src/Input.test.js
@@ -5,12 +5,13 @@ import { findByTestAttr, storeFactory } from '../test/testUtils';
 import Input from './Input';
 
 /**
- * Factory function to create a ShallowWrapper
+ * Factory function to create a ShallowWrapper in this connected component
  * @function setup
  * @param {object} initialState 
  */
 const setup = (initialState={}) => {
     const store = storeFactory(initialState);
+    //dive() recupera o componente da hoc
     const wrapper = shallow(<Input store={store}/>).dive().dive();
     //console.log(wrapper.debug());
     return wrapper;
@@ -21,7 +22,7 @@ const setup = (initialState={}) => {
 describe('render', () => {
     describe('word has NOT been guessed', () =>{
         test('renders component without error', () =>{
-
+            
         });
         test('renders input box', () =>{
 
@@ -31,14 +32,22 @@ describe('render', () => {
         });
     });
     describe('word has been guessed', () =>{
+        let wrapper;
+        beforeEach(() => {
+            const initialState = { success: false };
+            wrapper = setup(initialState);
+        });
         test('renders component without error', () =>{
-
+            const component = findByTestAttr(wrapper, "component-input");
+            expect(component.length).toBe(1);
         });
         test('NOT renders input box', () =>{
-
+            const inputBox = findByTestAttr(wrapper, "input-box");
+            expect(inputBox.length).toBe(1);
         });
         test('NOT renders submit button', () =>{
-
+            const submit = findByTestAttr(wrapper, "submit-button");
+            expect(submit.length).toBe(1);
         });
     });
 });
