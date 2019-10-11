76502a5053c7230d2edee4616c73ca5273c8ce72
Maisa Souza Barros <maisa.barros@fazenda.mg.gov.br>
2019-10-11 08:27:54 -0300

    Test case reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import { getLetterMatchCount } from '../helpers';
 
 export const actionTypes = {
     CORRECT_GUESS: 'CORRECT_GUESS',
@@ -6,7 +7,17 @@ export const actionTypes = {
 
 export const guessWord = (guessedWord) => {
     return (dispatch, getState) => {
+        const secretWord = getState().secretWord;
+        const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
 
+        dispatch({
+            type: actionTypes.GUESS_WORD,
+            payload: { guessedWord, letterMatchCount }
+        });
+
+        if (guessedWord === secretWord){
+            dispatch({ type: actionTypes.CORRECT_GUESS });
+        }
     };
 }
 
